f5d3b6e64ba13c96a1ebb8897fc1bc6a930ef183
mosama182 <m.osama182@gmail.com>
2024-11-18 12:28:37 +1100

    Some practice for coding

---
+++ binary_search.py
@@ -0,0 +1,61 @@
+def binary_search(array, target):
+
+    left = 0
+    right = len(array) - 1
+
+    while left <= right:
+
+        mid = (left + right) // 2
+
+        if array[mid] == target:
+            return mid
+        
+        elif target < array[mid]:
+            right = mid - 1
+
+        elif target > array[mid]:
+            left = mid + 1
+
+    
+    return -1
+
+
+def majority_element(array, left, right):
+
+    if left == right:
+        return array[left]
+    
+    mid = (left + right) // 2
+
+    left_majority = majority_element(array, left, mid)
+    right_majority = majority_element(array, mid + 1, right)
+
+    if left_majority == right_majority:
+        return left_majority
+    else:
+        left_count = count_occurence(array, left, mid, left_majority)
+        right_count = count_occurence(array, mid + 1, right, right_majority)
+
+        if left_count > right_count:
+            return left_majority
+        else:
+            return right_majority
+
+
+def count_occurence(array, left, right, majority):
+    """
+    Count the occurence of the element 'majority'
+    in the given array between indices left and right
+    """
+    
+    return array[left:right+1].count(majority)
+
+    
+if __name__ == "__main__":
+
+    #array = [0, 1, 2, 3, 4, 5]
+    #target = -1
+    #print(binary_search(array, target))
+
+    array = [2, 2, 3, 3, 3]
+    print(majority_element(array, 0, len(array)-1))
